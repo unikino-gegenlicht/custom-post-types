08d1e8eb79a2275f072edbea972d178222e57ad6
Jan Eike Suchard <jan@suchard.cloud>
2024-11-28 10:20:48 +0100

    init repo

---
+++ README.md
@@ -0,0 +1,21 @@
+<div align="center">
+    <img src="https://gegenlicht.net/wortmarke/">
+    <h1>Custom WordPress Post-Types and Taxonomies</h1>
+    <p>🧩 custom post types for our website</p>
+<img alt="License" src="https://img.shields.io/github/license/unikino-gegenlicht/custom-post-types?style=for-the-badge">
+
+<img alt="Release" src="https://img.shields.io/github/v/release/unikino-gegenlicht/custom-post-types?sort=semver&display_name=tag&style=for-the-badge">
+
+</div>
+
+> [!WARNING]
+> This WordPress plugin does not support automatic updates as it is supposed to used as a [must-use plugin].
+> 
+> [must-use plugin]: https://developer.wordpress.org/advanced-administration/plugins/mu-plugins/
+
+## Installation
+
+1. Download Current Release from the releases tab
+2. Create a folder called `mu-plugins` in your `wp-contents` folder
+3. Copy the current release directly into the `mu-plugins` directory
+4. Update your Permalink structure by just clicking the `Save` button in your WordPress permalink settings
---
+++ post-types.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * GEGENLICHT Custom Post Types
+ * 
+ * This plugin brings some new post types to WordPress installations.
+ * These post types are tailored to the usage at our student cinema but may be
+ * adapted by other cinemas and student organizations.
+ * 
+ * @package GGL_Post_Types
+ * @author Jan Eike Suchard
+ * @copyright 2024 Jan Eike Suchard
+ * @license EUPL-1.2
+ * 
+ * @wordpress-plugin
+ * Plugin Name:       Custom Post Types for Movies, Team Members and Supporters
+ * Plugin URI:        https://github.com/unikino-gegenlicht/custom-post-types
+ * Description:       This plugin introduces custom post types to the WordPress installation which enable handling of movies, team members and supporters
+ * Version:           1.0.0
+ * Requires at least: 6.1
+ * Requires PHP:      8.3
+ * Author:            Jan Eike Suchard
+ * Author URI:        https://suchard.cloud
+ * Text Domain:       ggl-post-types
+ * License:           EUPL-1.2
+ * License URI:       https://joinup.ec.europa.eu/sites/default/files/custom-page/attachment/2020-03/EUPL-1.2%20EN.txt
+ * Update URI:        false
+ */
