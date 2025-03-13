c5cf321ba552014da02dfb869262dad62dc6ae58
Jan Eike Suchard <jan@suchard.cloud>
2025-03-13 12:29:15 +0100

    chore: adding main plugin file

--- post-types.php
+++ post-types.php
@@ -18,7 +18,7 @@
  * Description:       This plugin introduces custom post types to the WordPress installation which enable handling of movies, team members and supporters
  * Version:           1.0.0
  * Requires at least: 6.1
- * Requires PHP:      8.3
+ * Requires PHP:      8.1
  * Author:            Jan Eike Suchard
  * Author URI:        https://suchard.cloud
  * Text Domain:       ggl-post-types
@@ -27,15 +27,65 @@
  * Update URI:        false
  */
 
-require_once 'post-types/supporters.php';
+require_once dirname(__FILE__) . '/inc/countries.php';
+require_once dirname(__FILE__) . '/inc/languages.php';
 
-add_action('init', 'ggl_post_type_supporter');
-add_filter( 'rwmb_meta_boxes', 'supporter_register_meta_boxes' );
+require_once(dirname(__FILE__) . "/functions.php");
+
+/* Register the taxonomies */
+require_once 'taxonomies/semester.php';
+unregister_taxonomy('semester');
+add_action('init', 'ggl_taxonomy_semester');
+add_filter('rwmb_meta_boxes', 'ggl_taxonomy_semester_meta_boxes');
 
 
+require_once 'taxonomies/program.php';
+unregister_taxonomy('program-type');
+add_action('init', 'ggl_taxonomy_program_type');
+add_filter('rwmb_meta_boxes', 'ggl_taxonomy_program_type_meta_boxes');
+
+require_once 'taxonomies/director.php';
+add_action('init', 'ggl_taxonomy_director');
+
+require_once 'taxonomies/actor.php';
+add_action('init', 'ggl_taxonomy_actor');
+
+require_once 'taxonomies/genre.php';
+add_action('init', 'ggl_taxonomy_genre');
+
+
+/* Register the post types */
 require_once 'post-types/movie.php';
- 
 add_action('init', 'ggl_post_type_movie');
 add_filter('wp_insert_post_data', 'movie_check_name_leaking', 10, 3);
 add_filter('rwmb_meta_boxes', 'movie_extended_info_meta_boxes');
+add_filter('rwmb_meta_boxes', 'movie_licensing_and_age_rating_meta_boxes');
+add_filter('rwmb_meta_boxes', 'movie_sound_information_meta_boxes');
 add_filter('rwmb_meta_boxes', 'movie_screening_info_meta_boxes');
+add_filter('rwmb_meta_boxes', 'movie_additional_information_box');
+add_filter('rwmb_meta_boxes', 'movie_short_movie_box');
+
+require_once 'post-types/event.php';
+add_action('init', 'ggl_post_type_event');
+add_filter('rwmb_meta_boxes', 'event_extended_info_meta_boxes');
+add_filter('rwmb_meta_boxes', 'event_sound_information_meta_boxes');
+add_filter('rwmb_meta_boxes', 'event_screening_info_meta_boxes');
+add_filter('rwmb_meta_boxes', 'event_additional_information_box');
+
+require_once 'post-types/supporters.php';
+add_action('init', 'ggl_post_type_supporter');
+add_filter('rwmb_meta_boxes', 'supporter_register_meta_boxes');
+
+require_once 'post-types/cooperation-partner.php';
+add_action('init', 'ggl_post_type_cooperation_partner');
+add_filter('rwmb_meta_boxes', 'cooperation_partner_register_meta_boxes');
+
+require_once 'post-types/team-member.php';
+add_action('init', 'ggl_post_type_team_member');
+add_filter('rwmb_meta_boxes', 'team_member_register_meta_boxes');
+
+
+add_action('admin_menu', 'reorder_menu');
+
+
+add_action('plugins_loaded', 'ggl_post_types_load_textdomain');
