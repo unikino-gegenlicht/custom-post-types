e7bf91b6c5b96f4e60cf69bdec690623019a0b8f
Jan Eike Suchard <60288203+dr4hcu5-jan@users.noreply.github.com>
2026-03-18 15:37:47 +0100

    chore: making generation of localized title aware of special programs

--- src/public-functions.php
+++ src/public-functions.php
@@ -114,26 +114,38 @@ function ggl_get_localized_title( int|WP_Post $post = 0 ): string {
 
 	// Check if the function shall return the real titles for the movie/event
 	$show_details = apply_filters( "ggl__show_full_details", false, $post );
+	if ( $show_details ) {
+		// as we only want to get the language for the localization we take a
+		// substring of the first two characters here as those are the
+		// language part of the BCP 47 tag that are returned by get_user_locale
+		$desired_language = substr( get_user_locale(), 0, 2 );
+		$meta_prefix      = match ( $desired_language ) {
+			"de" => "german",
+			default => "english"
+		};
 
-	if ( ! $show_details ) {
-		if ( $post->post_type === "event" ) {
-			return __( "An unnamed event", "ggl-post-types" );
-		} else {
-			return __( "An unnamed movie", "ggl-post-types" );
-		}
+		// now return the appropriate post metadata
+		return get_post_meta( $post->ID, $meta_prefix . "_title", true );
 	}
 
-	// as we only want to get the language for the localization we take a
-	// substring of the first two characters here as those are the
-	// language part of the BCP 47 tag that are returned by get_user_locale
-	$desired_language = substr( get_user_locale(), 0, 2 );
-	$meta_prefix      = match ( $desired_language ) {
-		"de" => "german",
-		default => "english"
-	};
+	if ( $post->post_type === "event" ) {
+		return __( "An unnamed event", "ggl-post-types" );
+	}
 
-	// now return the appropriate post metadata
-	return get_post_meta( $post->ID, $meta_prefix . "_title", true );
+
+	$is_in_special_program = get_post_meta( $post->ID, "program_type", true ) === "special_program";
+	if ( ! $is_in_special_program ) {
+		return get_post_meta( $post->ID, "original_title", true );
+	}
+
+	$assigned_special_program = array_first( wp_get_post_terms( $post->ID, "special-program" ) );
+	if ( $assigned_special_program === null ) {
+		return __( "An unnamed special", "ggl-post-types" );
+	} else {
+		return $assigned_special_program->name;
+	}
+
+
 }
 
 /**
